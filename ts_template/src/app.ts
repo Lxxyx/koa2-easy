dc7454737f4286e94393f12bfd7a6bec980743fc
Lxxyx <Lxxyxzj@gmail.com>
2018-03-28 20:43:55 +0800

    feat(TS): 升级TS依赖及启动方式

---
+++ ts_template/src/app.ts
@@ -0,0 +1,41 @@
+import Koa from 'koa'
+import bodyparser from 'koa-bodyparser'
+import compress from 'koa-compress'
+import convert from 'koa-convert'
+import cors from 'koa-cors'
+import logger from 'koa-logger'
+import api from './router/api'
+
+const app = new Koa()
+
+app.use(async (ctx, next) => {
+  ctx.set('PoweredBy', 'Koa2-Easy')
+  try {
+    await next()
+    ctx.body = {
+      code: 0,
+      message: 'ok',
+      data: JSON.parse(ctx.body)
+    }
+  } catch (e) {
+    ctx.status = e.status || 500
+    ctx.body = {
+      code: e.status || 500,
+      message: e.message,
+      data: {}
+    }
+  }
+})
+
+app.use(compress({ threshold: 2048 }))
+app.use(convert(cors()))
+app.use(bodyparser())
+app.use(convert(logger()))
+
+app.use(api.routes())
+
+const port = process.env.PORT ? Number(process.env.PORT) : 3000
+
+app.listen(port)
+
+console.log(`Now server is listen http://localhost:${port}`)
--- ts_template/src/router/api.ts
+++ ts_template/src/router/api.ts
@@ -1,4 +1,4 @@
-import * as Router from 'koa-router'
+import Router from 'koa-router'
 
 const api = new Router({ prefix: '/api' })
 
